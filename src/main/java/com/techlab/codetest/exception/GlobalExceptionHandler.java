f18a53d89f126721779a0535bd48eb3b7a47c79d
hyukjin <cdh5771@naver.com>
2023-10-09 21:03:30 +0900

    feat: validation exception handling 추가 구현

--- src/main/java/com/techlab/codetest/exception/GlobalExceptionHandler.java
+++ src/main/java/com/techlab/codetest/exception/GlobalExceptionHandler.java
@@ -1,8 +1,14 @@
 package com.techlab.codetest.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -21,5 +27,17 @@ public class GlobalExceptionHandler {
     public ResponseEntity handleConflictException(ApplicationException ex) {
         return ResponseEntity.status(ex.getHttpStatus())
                 .body(ex.getExceptionResponse());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
