b37b27a47b2e4ed9382cd264f6fa5a551770b439
hyukjin <cdh5771@naver.com>
2023-10-09 20:49:25 +0900

    style: optimize import

--- src/main/java/com/techlab/codetest/dto/request/RecUpdateRequest.java
+++ src/main/java/com/techlab/codetest/dto/request/RecUpdateRequest.java
@@ -7,7 +7,6 @@ import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
-import javax.validation.constraints.PositiveOrZero;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
